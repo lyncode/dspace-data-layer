a907c674fa904f6b1bac49226cff52ffa18d4d9f
Lyncode <development@lyncode.com>
2012-08-16 15:56:25 +0100

    org.dspace.orm.model -> org.dspace.orm.entity

---
+++ src/main/java/org/dspace/orm/entity/ChecksumHistory.java
@@ -0,0 +1,13 @@
+package org.dspace.orm.entity;
+
+import org.springframework.roo.addon.dbre.RooDbManaged;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(versionField = "", table = "checksum_history", schema = "public")
+@RooDbManaged(automaticallyDelete = true)
+public class ChecksumHistory {
+}
