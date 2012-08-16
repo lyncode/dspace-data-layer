97fffe77e707450f26892849f4de7773c68bd252
Lyncode <development@lyncode.com>
2012-08-16 03:06:32 +0100

    Initial commit
    Spring Roo reverse engeneering generated files

---
+++ src/main/java/org/dspace/orm/model/Community2community.java
@@ -0,0 +1,13 @@
+package org.dspace.orm.model;
+
+import org.springframework.roo.addon.dbre.RooDbManaged;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(versionField = "", table = "community2community", schema = "public")
+@RooDbManaged(automaticallyDelete = true)
+public class Community2community {
+}
